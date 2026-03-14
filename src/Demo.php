93406509391af69053423a39cf86ed00691ed751
Thorsten Schneider <mr.thorsten.schneider@outlook.com>
2026-03-14 20:00:05 +0100

    Create version 3.0.0

--- src/Demo.php
+++ src/Demo.php
@@ -6,6 +6,6 @@ class Demo
 {
     function printSomething()
     {
-        return 'This comes from ts160876\ComposerDemo1 (v.2.0.0)';
+        return 'This comes from ts160876\ComposerDemo1 (v.3.0.0)';
     }
 }
